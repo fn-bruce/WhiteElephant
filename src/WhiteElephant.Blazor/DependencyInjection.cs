c87dc23808e651676da8b7f5933dbbbb3bfc7148
Bruce Nguyen <bruceleenguyen95@gmail.com>
2023-12-13 10:28:23 -0700

    feat: add data into local storage instead of in-memory

--- src/WhiteElephant.Blazor/DependencyInjection.cs
+++ src/WhiteElephant.Blazor/DependencyInjection.cs
@@ -1,9 +1,12 @@
+using Blazored.LocalStorage;
+
 namespace WhiteElephant.Blazor;
 
 public static class DependencyInjection
 {
     public static IServiceCollection AddWebServices(this IServiceCollection services)
     { 
+        services.AddBlazoredLocalStorage();
         services.AddRazorComponents().AddInteractiveServerComponents();
 
         return services;
--- src/WhiteElephant.Infrastructure/DependencyInjection.cs
+++ src/WhiteElephant.Infrastructure/DependencyInjection.cs
@@ -8,7 +8,7 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
     {
-        services.AddSingleton<IPersonRepository, PersonRepository>();
+        services.AddScoped<IPersonRepository, PersonRepository>();
         
         return services;
     }
